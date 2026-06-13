fccb11204abc628344731422e53c4246ae8cb435
Ali | Software Engineer & AI Architect <ly4590087@gmail.com>
2026-06-13 22:56:03 +0300

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: '*',
       allow: '/',
     },
-    sitemap: 'https://carstyle-sa.com/sitemap.xml',
+    sitemap: 'https://www.carstyle-sa.com/sitemap.xml',
   }
 }
